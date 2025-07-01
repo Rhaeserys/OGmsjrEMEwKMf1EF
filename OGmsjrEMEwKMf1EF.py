516e9780cfb73309a2e32b26189f7090ae353023
Ra Ci-decant <117764959+Rhaeserys@users.noreply.github.com>
2025-07-01 16:09:00 -0500

    Update OGmsjrEMEwKMf1EF.py

--- OGmsjrEMEwKMf1EF.py
+++ OGmsjrEMEwKMf1EF.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split, cross_val_score, StratifieldKFold
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
 from sklearn.preprocessing import StandarScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
